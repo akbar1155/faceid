8c4ba9f4bbd1c62aae072e67cbd3de0143aae620
akbar1155 <isroilovakbar49@gmail.com>
2024-12-05 09:57:20 +0500

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
 import { useEffect, useRef, useState } from 'react';
-import exclamation from "./assets/image.png"
+import exclamation from "./assets/image.png";
 import './App.css';
+
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const iframeRef = useRef<HTMLIFrameElement>(null);
+
   useEffect(() => {
     const handleMessage = (e: MessageEvent) => {
-      console.log(e.data);
-      if (e.data.cmd === -1 && e.data.message === 'ready') {
+      if (e.origin !== "https://docs.myid.uz") return; // Xavfsizlik uchun tekshirish
+      if (e.data?.cmd === -1 && e.data?.message === 'ready') {
         openCamera();
       }
     };
+
     window.addEventListener('message', handleMessage);
 
     return () => {
       window.removeEventListener('message', handleMessage);
     };
   }, []);
+
   const openCamera = () => {
     if (iframeRef.current) {
-      iframeRef.current.contentWindow?.postMessage({ cmd: 'open', config: { locale: 'uz' } }, '*');
+      iframeRef.current.contentWindow?.postMessage(
+        { cmd: 'open', config: { locale: 'uz' } },
+        "https://docs.myid.uz" // Aniq URL
+      );
     }
   };
+
   const closeCamera = () => {
     if (iframeRef.current) {
-      iframeRef.current.contentWindow?.postMessage({ cmd: 'close' }, '*');
+      iframeRef.current.contentWindow?.postMessage(
+        { cmd: 'close' },
+        "https://docs.myid.uz"
+      );
     }
   };
+
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
@@ -34,7 +46,7 @@ function App() {
   return (
     <div className="container">
       <iframe
-        className='faceid'
+        className="faceid"
         ref={iframeRef}
         id="myid_iframe"
         src="https://docs.myid.uz/iframe"
@@ -43,9 +55,7 @@ function App() {
       ></iframe>
       <button onClick={openCamera}>Open Camera</button>
       <button onClick={closeCamera}>Close Camera</button>
-      <button onClick={toggleModal}>
-        Info
-      </button>
+      <button onClick={toggleModal}>Info</button>
       {isModalOpen && (
         <div className="modal">
           <div className="modal-content">
@@ -57,4 +67,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
